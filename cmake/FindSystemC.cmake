094a17407f2f686d479078895b90f8cf5d4900bd
Jan Henrik Weinstock <jan.weinstock@rwth-aachen.de>
2019-05-02 17:04:31 +0200

    Check for old style TLM include directory

--- cmake/FindSystemC.cmake
+++ cmake/FindSystemC.cmake
@@ -45,8 +45,11 @@ find_library(SYSTEMC_LIBRARY NAMES libsystemc.a systemc libSnpsVP.so
                    ${SYSTEMC_HOME}/libso-gcc-6.2.0-64
                    ${SYSTEMC_HOME}/libso-gcc-5.2.0-64)
 
-set(SYSTEMC_INCLUDE_DIRS ${SYSTEMC_INCLUDE_DIR} ${SYSTEMC_INCLUDE_DIR}/tlm)
-set(SYSTEMC_LIBRARIES "${SYSTEMC_LIBRARY}")
+set(SYSTEMC_LIBRARIES ${SYSTEMC_LIBRARY})
+set(SYSTEMC_INCLUDE_DIRS ${SYSTEMC_INCLUDE_DIR})
+if(EXISTS ${SYSTEMC_INCLUDE_DIR}/tlm/)
+    list(APPEND SYSTEMC_INCLUDE_DIRS ${SYSTEMC_INCLUDE_DIR}/tlm)
+endif()
 
 set(_sysc_ver_file "${SYSTEMC_INCLUDE_DIR}/sysc/kernel/sc_ver.h")
 if(EXISTS ${_sysc_ver_file})
