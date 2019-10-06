0242b3e6f82b90691d974b97f3d437a34409dc97
Jan Henrik Weinstock <jan.weinstock@rwth-aachen.de>
2019-10-06 10:04:22 +0200

    Added stubbable input ports

--- include/vcml/ports.h
+++ include/vcml/ports.h
@@ -27,23 +27,104 @@
 namespace vcml {
 
     template <typename T>
+    class stub_signal: public sc_core::sc_signal_inout_if<T>
+    {
+    private:
+        T m_retval;
+        sc_event m_never;
+
+    public:
+        stub_signal(sc_object* parent, const T& retval = T()):
+            m_retval(retval),
+            m_never(concat(parent->basename(), "_never_ev").c_str()) {
+        }
+
+        virtual const sc_event& value_changed_event() const {
+            return m_never;
+        }
+
+        virtual const sc_event& default_event() const { return m_never; }
+        virtual const sc_event& posedge_event() const { return m_never; }
+        virtual const sc_event& negedge_event() const { return m_never; }
+
+        virtual void write(const T& val) { /* ignored */ }
+        virtual const T& read() const { return m_retval; }
+        virtual const T& get_data_ref() const { return m_retval; }
+
+        virtual bool event()   const { return false; }
+        virtual bool posedge() const { return false; }
+        virtual bool negedge() const { return false; }
+    };
+
+    template <typename T>
+    class in_port: public sc_in<T>
+    {
+    private:
+        sc_module* m_parent;
+        typename sc_in<T>::if_type* m_stub;
+
+    public:
+        in_port() = delete;
+        in_port(const in_port<T>&) = delete;
+
+        in_port(const char* name);
+        virtual ~in_port();
+        VCML_KIND(in_port);
+
+        bool is_stubbed() const;
+        void stub(const T& retval = T());
+    };
+
+    template <typename T>
+    in_port<T>::in_port(const char* nm):
+        sc_core::sc_in<T>(nm),
+        m_parent(dynamic_cast<sc_module*>(sc_object::get_parent_object())),
+        m_stub(nullptr) {
+        VCML_ERROR_ON(!m_parent, "%s outside module", sc_in<T>::name());
+    }
+
+    template <typename T>
+    in_port<T>::~in_port() {
+        if (m_stub != nullptr)
+            delete m_stub;
+    }
+
+    template <typename T>
+    bool in_port<T>::is_stubbed() const {
+        return m_stub != nullptr;
+    }
+
+    template <typename T>
+    void in_port<T>::stub(const T& retval) {
+        VCML_ERROR_ON(!is_stubbed(), "%s already stubbed", sc_in<T>::name());
+
+        sc_simcontext* simc = sc_object::simcontext();
+        simc->hierarchy_push(m_parent);
+        m_stub = new stub_signal<T>(this, retval);
+        sc_in<T>::bind(*m_stub);
+        simc->hierarchy_pop();
+    }
+
+    template <typename T>
     class out_port: public sc_out<T>
     {
     private:
         T m_state;
         sc_event m_update;
-        sc_signal<T>* m_stub;
+        sc_module* m_parent;
+
+        typename sc_out<T>::if_type* m_stub;
 
         void update();
 
     public:
-        out_port();
-        out_port(const sc_module_name& name);
-        virtual ~out_port();
-        VCML_KIND(out_port);
-
+        out_port() = delete;
         out_port(const out_port<T>& copy) = delete;
         out_port(out_port<T>&& other) = delete;
+
+        out_port(const char* name);
+        virtual ~out_port();
+        VCML_KIND(out_port);
 
         out_port<T>& operator = (T val);
 
@@ -61,27 +142,14 @@ namespace vcml {
     }
 
     template <typename T>
-    out_port<T>::out_port():
-        sc_core::sc_out<T>(sc_gen_unique_name("out")),
-        m_state(),
-        m_update(concat(this->basename(), "_update_ev").c_str()),
-        m_stub(nullptr) {
-        sc_core::sc_spawn_options opts;
-        opts.spawn_method();
-        opts.set_sensitivity(&m_update);
-        opts.dont_initialize();
-
-        sc_spawn(sc_bind(&out_port<T>::update, this),
-                 concat(this->basename(), "_update").c_str(),
-                  &opts);
-    }
-
-    template <typename T>
-    out_port<T>::out_port(const sc_module_name& nm):
+    out_port<T>::out_port(const char* nm):
         sc_core::sc_out<T>(nm),
         m_state(false),
         m_update(concat(this->basename(), "_update_ev").c_str()),
+        m_parent(dynamic_cast<sc_module*>(sc_object::get_parent_object())),
         m_stub(nullptr) {
+        VCML_ERROR_ON(!m_parent, "%s outside module", sc_out<T>::name());
+
         sc_core::sc_spawn_options opts;
         opts.spawn_method();
         opts.set_sensitivity(&m_update);
@@ -133,16 +201,12 @@ namespace vcml {
 
     template <typename T>
     void out_port<T>::stub() {
-        if (is_stubbed())
-            return;
+        VCML_ERROR_ON(!is_stubbed(), "%s already stubbed", sc_out<T>::name());
 
         sc_simcontext* simc = sc_object::simcontext();
-        sc_module* p = dynamic_cast<sc_module*>(sc_object::get_parent_object());
-        VCML_ERROR_ON(!p, "failed to lookup parent module");
-
-        simc->hierarchy_push(p);
-        m_stub = new sc_signal<T>(concat(this->basename(), "_stub").c_str());
-        bind(*m_stub);
+        simc->hierarchy_push(m_parent);
+        m_stub = new sc_signal<T>(this);
+        sc_out<T>::bind(*m_stub);
         simc->hierarchy_pop();
     }
 
@@ -220,7 +284,7 @@ namespace vcml {
     using out_port_list = port_list<out_port<T>>;
 
     template <typename T>
-    using in_port_list = port_list<sc_in<T>>;
+    using in_port_list = port_list<in_port<T>>;
 
 }
 
