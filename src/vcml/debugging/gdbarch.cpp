f10fa0793f6aa70b99f04faff322ab329446d879
Antonios Salios <salios@ice.rwth-aachen.de>
2021-10-07 14:13:33 +0200

    Fix order of registers in rsp communication

--- src/vcml/debugging/gdbarch.cpp
+++ src/vcml/debugging/gdbarch.cpp
@@ -160,7 +160,7 @@ namespace vcml { namespace debugging {
             "r8",  "r9",  "r10", "r11", "r12", "r13", "r14", "r15",
             "r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23",
             "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31",
-            "ppc", "npc", "sr" }},
+            "npc", "sr", "ppc" }},
     });
 
     const gdbarch gdbarch_riscv("riscv", "riscv", "none", {
