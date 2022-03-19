4d110ca4709b2defa8845d1c0b56e8e9a29d4063
Jan Henrik Weinstock <jan.weinstock@rwth-aachen.de>
2022-03-19 12:14:26 +0100

    Use find_package also for cmake included systemc

--- cmake/FindSystemC.cmake
+++ cmake/FindSystemC.cmake
@@ -21,7 +21,6 @@ if(TARGET systemc)
     return()
 endif()
 
-# TARGET_ARCH
 if(DEFINED ENV{TARGET_ARCH})
     set(SYSTEMC_TARGET_ARCH $ENV{TARGET_ARCH})
 elseif(DEFINED TARGET_ARCH)
@@ -32,7 +31,6 @@ else()
     set(SYSTEMC_TARGET_ARCH "linux")
 endif()
 
-# SYSTEMC_HOME
 if(NOT EXISTS ${SYSTEMC_HOME})
     set(SYSTEMC_HOME $ENV{SYSTEMC_HOME})
 endif()
@@ -41,66 +39,57 @@ if(NOT EXISTS ${SYSTEMC_HOME})
     message(FATAL_ERROR "Could not find SystemC")
 endif()
 
-macro(sysc_version _sysc_ver_file)
-    if(EXISTS ${_sysc_ver_file})
-        file(STRINGS ${_sysc_ver_file}  _systemc_ver REGEX
-        "^#[\t ]*define[\t ]+SC_VERSION_(MAJOR|MINOR|PATCH)[\t ]+([0-9]+)$")
-        foreach(VPART MAJOR MINOR PATCH)
-            foreach(VLINE ${_systemc_ver})
-                if(VLINE MATCHES
-                   "^#[\t ]*define[\t ]+SC_VERSION_${VPART}[\t ]+([0-9]+)$")
-                    set(SYSTEMC_VERSION_${VPART} ${CMAKE_MATCH_1})
-                    if(SYSTEMC_VERSION)
-                        string(APPEND SYSTEMC_VERSION .${CMAKE_MATCH_1})
-                    else()
-                        set(SYSTEMC_VERSION ${CMAKE_MATCH_1})
-                    endif()
-                endif()
-            endforeach()
-        endforeach()
-    endif()
-endmacro()
-
 if(EXISTS ${SYSTEMC_HOME}/CMakeLists.txt)
-    message(STATUS "Found SystemC at " ${SYSTEMC_HOME})
     set(ENABLE_PHASE_CALLBACKS ON CACHE STRING "")
     set(ENABLE_PHASE_CALLBACKS_TRACING ON CACHE STRING "")
     add_subdirectory(${SYSTEMC_HOME} systemc EXCLUDE_FROM_ALL)
     set(SYSTEMC_LIBRARIES systemc)
     set(SYSTEMC_INCLUDE_DIRS $<TARGET_PROPERTY:systemc,INCLUDE_DIRECTORIES>)
-    set(SYSTEMC_VERSION "")
-    sysc_version(${SYSTEMC_HOME}/src/sysc/kernel/sc_ver.h)
 else()
-    find_path(SYSTEMC_INCLUDE_DIR NAMES systemc
+    find_path(SYSTEMC_INCLUDE_DIRS NAMES systemc
               HINTS ${SYSTEMC_HOME}/include)
 
-    find_library(SYSTEMC_LIBRARY NAMES libsystemc.a systemc
+    find_library(SYSTEMC_LIBRARIES NAMES libsystemc.a systemc
                  HINTS ${SYSTEMC_HOME}/lib-${SYSTEMC_TARGET_ARCH}
                        ${SYSTEMC_HOME}/lib)
 
-    set(SYSTEMC_LIBRARIES ${SYSTEMC_LIBRARY})
-    set(SYSTEMC_INCLUDE_DIRS ${SYSTEMC_INCLUDE_DIR})
-
-    if(EXISTS ${SYSTEMC_INCLUDE_DIR}/tlm/)
-        list(APPEND SYSTEMC_INCLUDE_DIRS ${SYSTEMC_INCLUDE_DIR}/tlm)
+    if(EXISTS ${SYSTEMC_INCLUDE_DIRS}/tlm/)
+        list(APPEND SYSTEMC_INCLUDE_DIRS ${SYSTEMC_INCLUDE_DIRS}/tlm)
     endif()
-
-    set(SYSTEMC_VERSION "")
-    sysc_version(${SYSTEMC_INCLUDE_DIR}/sysc/kernel/sc_ver.h)
-
-    include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(SystemC
-        REQUIRED_VARS SYSTEMC_LIBRARY SYSTEMC_INCLUDE_DIR
-        VERSION_VAR   SYSTEMC_VERSION)
-
-    mark_as_advanced(SYSTEMC_INCLUDE_DIR SYSTEMC_LIBRARY)
 endif()
+
+set(SYSTEMC_VERSION "")
+file(GLOB_RECURSE _sysc_ver_file ${SYSTEMC_HOME}/*/sysc/kernel/sc_ver.h)
+if(NOT EXISTS ${_sysc_ver_file})
+    message(FATAL_ERROR "Cannot find sc_ver.h")
+endif()
+
+file(STRINGS ${_sysc_ver_file} _systemc_ver REGEX
+     "^#[\t ]*define[\t ]+SC_VERSION_(MAJOR|MINOR|PATCH)[\t ]+([0-9]+)$")
+foreach(VPART MAJOR MINOR PATCH)
+    foreach(VLINE ${_systemc_ver})
+        if(VLINE MATCHES 
+           "^#[\t ]*define[\t ]+SC_VERSION_${VPART}[\t ]+([0-9]+)$")
+            set(SYSTEMC_VERSION_${VPART} ${CMAKE_MATCH_1})
+            if(SYSTEMC_VERSION)
+                string(APPEND SYSTEMC_VERSION .${CMAKE_MATCH_1})
+            else()
+                set(SYSTEMC_VERSION ${CMAKE_MATCH_1})
+            endif()
+        endif()
+    endforeach()
+endforeach()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SystemC
+        REQUIRED_VARS SYSTEMC_LIBRARIES SYSTEMC_INCLUDE_DIRS
+        VERSION_VAR   SYSTEMC_VERSION)
+mark_as_advanced(SYSTEMC_LIBRARIES SYSTEMC_INCLUDE_DIRS)
 
 #message(STATUS "SYSTEMC_FOUND         " ${SYSTEMC_FOUND})
 #message(STATUS "SYSTEMC_HOME          " ${SYSTEMC_HOME})
 #message(STATUS "SYSTEMC_TARGET_ARCH   " ${SYSTEMC_TARGET_ARCH})
 #message(STATUS "SYSTEMC_INCLUDE_DIRS  " ${SYSTEMC_INCLUDE_DIRS})
-#message(STATUS "SYSTEMC_LIBRARY       " ${SYSTEMC_LIBRARY})
 #message(STATUS "SYSTEMC_LIBRARIES     " ${SYSTEMC_LIBRARIES})
 #message(STATUS "SYSTEMC_VERSION_MAJOR " ${SYSTEMC_VERSION_MAJOR})
 #message(STATUS "SYSTEMC_VERSION_MINOR " ${SYSTEMC_VERSION_MINOR})
