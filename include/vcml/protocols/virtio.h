425736fab00e7940f00b13fe9a55e21612a65a11
Jan Henrik Weinstock <jan.weinstock@rwth-aachen.de>
2022-04-08 23:11:06 +0200

    Adding device ID for virtio p9fs

--- include/vcml/protocols/virtio.h
+++ include/vcml/protocols/virtio.h
@@ -56,6 +56,7 @@ enum virtio_devices : u32 {
     VIRTIO_DEVICE_BLOCK   = 2,
     VIRTIO_DEVICE_CONSOLE = 3,
     VIRTIO_DEVICE_RNG     = 4,
+    VIRTIO_DEVICE_P9FS    = 9,
     VIRTIO_DEVICE_GPU     = 16,
     VIRTIO_DEVICE_INPUT   = 18,
 };
