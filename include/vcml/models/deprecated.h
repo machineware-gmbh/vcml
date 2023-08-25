83224b79ffe32188218f0a88ee12d615141aa0e5
Jan Henrik Weinstock <jan@mwa.re>
2023-08-25 13:54:52 +0200

    Re-enable deprecation warnings

--- include/vcml/models/deprecated.h
+++ include/vcml/models/deprecated.h
@@ -13,17 +13,17 @@
 
 #define VCML_MODEL_DEPRECATED(oldns, oldname, newns, newname) \
     namespace oldns {                                         \
-    typedef ::newns::newname oldname MWR_DECL_DEPRECATED;     \
+    typedef MWR_DECL_DEPRECATED ::newns::newname oldname;     \
     }
 
-// VCML_MODEL_DEPRECATED(vcml::generic, rtc1742, vcml::timers, rtc1742)
-// VCML_MODEL_DEPRECATED(vcml::generic, lan9118, vcml::ethernet, lan9118)
-// VCML_MODEL_DEPRECATED(vcml::generic, uart8250, vcml::serial, uart8250)
-// VCML_MODEL_DEPRECATED(vcml::sd, sdcard, vcml::sd, card)
-// VCML_MODEL_DEPRECATED(vcml::opencores, ethoc, vcml::ethernet, ethoc)
-// VCML_MODEL_DEPRECATED(vcml::opencores, ocspi, vcml::spi, ocspi)
-// VCML_MODEL_DEPRECATED(vcml::opencores, oci2c, vcml::i2c, oci2c)
-// VCML_MODEL_DEPRECATED(vcml::arm, pl011uart, vcml::serial, pl011)
-// VCML_MODEL_DEPRECATED(vcml::arm, sp804timer, vcml::timers, sp804)
+VCML_MODEL_DEPRECATED(vcml::generic, rtc1742, vcml::timers, rtc1742)
+VCML_MODEL_DEPRECATED(vcml::generic, lan9118, vcml::ethernet, lan9118)
+VCML_MODEL_DEPRECATED(vcml::generic, uart8250, vcml::serial, uart8250)
+VCML_MODEL_DEPRECATED(vcml::sd, sdcard, vcml::sd, card)
+VCML_MODEL_DEPRECATED(vcml::opencores, ethoc, vcml::ethernet, ethoc)
+VCML_MODEL_DEPRECATED(vcml::opencores, ocspi, vcml::spi, ocspi)
+VCML_MODEL_DEPRECATED(vcml::opencores, oci2c, vcml::i2c, oci2c)
+VCML_MODEL_DEPRECATED(vcml::arm, pl011uart, vcml::serial, pl011)
+VCML_MODEL_DEPRECATED(vcml::arm, sp804timer, vcml::timers, sp804)
 
 #endif
