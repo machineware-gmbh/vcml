4d19b7a9243ad5fe7084c0a3145dffecff345a57
Jan Henrik Weinstock <jan@mwa.re>
2023-12-06 19:10:46 +0100

    Adding peq and test

---
+++ include/vcml/core/peq.h
@@ -0,0 +1,93 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2023 MachineWare GmbH                                        *
+ * All Rights Reserved                                                        *
+ *                                                                            *
+ * This is work is licensed under the terms described in the LICENSE file     *
+ * found in the root directory of this source tree.                           *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef VCML_PEQ_H
+#define VCML_PEQ_H
+
+#include "vcml/core/types.h"
+#include "vcml/core/systemc.h"
+
+namespace vcml {
+
+template <typename T>
+class peq : public sc_object
+{
+private:
+    sc_event m_event;
+    std::multimap<sc_time, T> m_schedule;
+
+public:
+    // Constructor
+    peq(const char* nm):
+        sc_object(nm),
+        m_event(mkstr("%s_event", basename()).c_str()),
+        m_schedule() {}
+    virtual ~peq() = default;
+    VCML_KIND(peq);
+
+    void notify(T payload, double t, sc_time_unit tu);
+    void notify(T payload, const sc_time& delta);
+    void cancel(T obj);
+    void wait(T& obj);
+
+    sc_event& event() { return m_event; }
+};
+
+template <typename T>
+inline void peq<T>::notify(T payload, double t, sc_time_unit tu) {
+    notify(payload, sc_time(t, tu));
+}
+
+template <typename T>
+inline void peq<T>::notify(T payload, const sc_time& delta) {
+    sc_time t = sc_time_stamp() + delta;
+    m_schedule.emplace(t, payload);
+    m_event.notify(m_schedule.begin()->first - sc_time_stamp());
+}
+
+template <typename T>
+inline void peq<T>::cancel(T payload) {
+    if (m_schedule.empty())
+        return;
+
+    sc_time curr = m_schedule.begin()->first;
+    mwr::stl_remove(m_schedule, payload);
+
+    if (m_schedule.empty()) {
+        m_event.cancel();
+        return;
+    }
+
+    sc_time next = m_schedule.begin()->first;
+    if (next == curr)
+        return;
+
+    m_event.cancel();
+    m_event.notify(next - sc_time_stamp());
+}
+
+template <typename T>
+inline void peq<T>::wait(T& obj) {
+    auto it = m_schedule.find(sc_time_stamp());
+    while (it == m_schedule.end()) {
+        sc_core::wait(m_event);
+        it = m_schedule.find(sc_time_stamp());
+    }
+
+    obj = it->second;
+    m_schedule.erase(it);
+
+    if (!m_schedule.empty())
+        m_event.notify(m_schedule.begin()->first - sc_time_stamp());
+}
+
+} // namespace vcml
+
+#endif
---
+++ test/core/peq.cpp
@@ -0,0 +1,65 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2023 MachineWare GmbH                                        *
+ * All Rights Reserved                                                        *
+ *                                                                            *
+ * This is work is licensed under the terms described in the LICENSE file     *
+ * found in the root directory of this source tree.                           *
+ *                                                                            *
+ ******************************************************************************/
+
+#include "testing.h"
+
+#include "vcml/core/peq.h"
+
+class peq_test : public test_base
+{
+public:
+    peq_test(const sc_module_name& nm): test_base(nm) {}
+    virtual ~peq_test() = default;
+
+    virtual void run_test() override {
+        int val;
+        peq<int> queue("peq");
+
+        EXPECT_STREQ(queue.name(), "test.run.peq");
+        EXPECT_STREQ(queue.event().name(), "test.run.peq_event");
+        EXPECT_STREQ(queue.kind(), "vcml::peq");
+
+        queue.notify(2, 2.0, SC_SEC);
+        queue.notify(1, 1.0, SC_SEC);
+        queue.notify(3, 3.0, SC_SEC);
+
+        queue.wait(val);
+        EXPECT_EQ(val, 1);
+        EXPECT_EQ(sc_time_stamp(), sc_time(1.0, SC_SEC));
+
+        queue.notify(4, 2.0, SC_SEC);
+
+        queue.wait(val);
+        EXPECT_EQ(val, 2);
+        EXPECT_EQ(sc_time_stamp(), sc_time(2.0, SC_SEC));
+
+        queue.wait(val);
+        EXPECT_EQ(val, 3);
+        EXPECT_EQ(sc_time_stamp(), sc_time(3.0, SC_SEC));
+
+        queue.wait(val);
+        EXPECT_EQ(val, 4);
+        EXPECT_EQ(sc_time_stamp(), sc_time(3.0, SC_SEC));
+
+        queue.notify(5, 1.0, SC_SEC);
+        queue.notify(6, 2.0, SC_SEC);
+        queue.cancel(5);
+        queue.cancel(99);
+
+        queue.wait(val);
+        EXPECT_EQ(val, 6);
+        EXPECT_EQ(sc_time_stamp(), sc_time(5.0, SC_SEC));
+    }
+};
+
+TEST(peq, test) {
+    peq_test test("test");
+    sc_core::sc_start();
+}
