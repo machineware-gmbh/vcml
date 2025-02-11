1e1801cc8c0b827bc3c18636d0e6e2372993b576
Antonios Salios <antonios@mwa.re>
2025-02-11 14:04:51 -0800

    feat(build): add msvc powershell script

---
+++ utils/setup-msvc.ps1
@@ -0,0 +1,35 @@
+ ##############################################################################
+ #                                                                            #
+ # Copyright (C) 2022 MachineWare GmbH                                        #
+ # All Rights Reserved                                                        #
+ #                                                                            #
+ # This is work is licensed under the terms described in the LICENSE file     #
+ # found in the root directory of this source tree.                           #
+ #                                                                            #
+ ##############################################################################
+
+$ErrorActionPreference = 'Stop'
+ 
+$VCML_HOME = (Get-Item $PSScriptRoot).parent.FullName
+$BINDIR = "$VCML_HOME\BUILD"
+$BUILD = "$BINDIR\.BUILD"
+$NPROC = (Get-WmiObject -Class Win32_Processor).NumberOfCores
+
+if ($args.Length) {
+    $BUILDS = $args
+}
+else {
+    $BUILDS = "DEBUG", "RELEASE"
+}
+
+cmake -B "$BUILD" -G "Visual Studio 17" `
+    -DCMAKE_INSTALL_PREFIX="$BINDIR" `
+    -DCMAKE_EXPORT_COMPILE_COMMANDS="ON" `
+    -DVCML_BUILD_TESTS="ON" `
+    -DVCML_BUILD_UTILS="ON" `
+    "$VCML_HOME"
+
+foreach ($TYPE in $BUILDS) {
+    cmake --build $BUILD --config $TYPE -j $NPROC
+    cmake --install $BUILD --config $TYPE
+}
