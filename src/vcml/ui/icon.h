a72939dc04be0f75b53a21bdbec7217e36d51b22
Jan Henrik Weinstock <jan@mwa.re>
2025-04-02 11:06:28 +0200

    adding icon

---
+++ src/vcml/ui/icon.h
@@ -0,0 +1,24 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (C) 2024 MachineWare GmbH                                        *
+ * All Rights Reserved                                                        *
+ *                                                                            *
+ * This is work is licensed under the terms described in the LICENSE file     *
+ * found in the root directory of this source tree.                           *
+ *                                                                            *
+ ******************************************************************************/
+
+#ifndef VCML_UI_ICON_H
+#define VCML_UI_ICON_H
+
+namespace vcml {
+namespace ui {
+
+static const unsigned int ICON_WIDTH = 64;
+static const unsigned int ICON_HEIGHT = 64;
+extern const unsigned int ICON_DATA[ICON_WIDTH * ICON_HEIGHT];
+
+} // namespace ui
+} // namespace vcml
+
+#endif
