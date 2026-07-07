b3e127233bfe480a0b71522e76846c015be6c381
Jan Henrik Weinstock <jan@mwa.re>
2026-07-07 07:29:38 +0200

    chore: fix typo in copyright headers

--- include/vcml/models/usb/keyboard.h
+++ include/vcml/models/usb/keyboard.h
@@ -3,8 +3,8 @@
  * Copyright (C) 2024 MachineWare GmbH                                        *
  * All Rights Reserved                                                        *
  *                                                                            *
- * This is work is licensed under the terms described in the LICENSE file     *
- * found in the root directory of this source tree.                           *
+ * This work is licensed under the terms described in the LICENSE file found  *
+ * in the root directory of this source tree.                                 *
  *                                                                            *
  ******************************************************************************/
 
