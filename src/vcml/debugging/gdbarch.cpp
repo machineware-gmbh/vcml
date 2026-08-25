88984d3e5a80bae5d5b8ea9e56d5fa2486b67aef
Niko Zurstraßen <67646205+not-chciken@users.noreply.github.com>
2026-08-25 12:41:06 +0200

    feat: adding avr gdbarch (#193)

--- src/vcml/debugging/gdbarch.cpp
+++ src/vcml/debugging/gdbarch.cpp
@@ -206,6 +206,16 @@ const gdbarch gdbarch::ARM_M = gdbarch(
             "r11", "r12", "sp", "lr", "pc", "xpsr" } },
     });
 
+const gdbarch gdbarch::AVR = gdbarch(
+    "avr", "avr", "none",
+    {
+        { "org.gnu.gdb.avr.core",
+          { "r0",  "r1",  "r2",  "r3",  "r4",  "r5",   "r6",  "r7",  "r8",
+            "r9",  "r10", "r11", "r12", "r13", "r14",  "r15", "r16", "r17",
+            "r18", "r19", "r20", "r21", "r22", "r23",  "r24", "r25", "r26",
+            "r27", "r28", "r29", "r30", "r31", "sreg", "sp",  "pc2" } },
+    });
+
 const gdbarch gdbarch::OR1K = gdbarch(
     "or1k", "or1k", "none",
     {
