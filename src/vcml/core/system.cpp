eee8757231ffc6216c2e391539a8d54dc5a4fbfe
Niko Zurstraßen <niko@mwa.re>
2026-09-14 11:14:22 +0200

    updating variable names

--- src/vcml/core/system.cpp
+++ src/vcml/core/system.cpp
@@ -130,13 +130,13 @@ int system::run() {
             log_info("simulation stopped");
         }
 
-        double realtime = mwr::timestamp() - simstart;
-        double duration = sc_core::sc_time_stamp().to_seconds();
+        double wall_clock_time = mwr::timestamp() - simstart;
+        double systemc_time = sc_core::sc_time_stamp().to_seconds();
         setlocale(LC_ALL, "");
-        log_info("elapsed SystemC time:      %.9fs", duration);
-        log_info("elapsed wall-clock time:   %.3fs", realtime);
+        log_info("elapsed SystemC time:      %.9fs", systemc_time);
+        log_info("elapsed wall-clock time:   %.3fs", wall_clock_time);
         log_info("simulation realtime ratio: %.2fs / 1s",
-                 duration == 0.0 ? 0.0 : realtime / duration);
+                 duration == 0.0 ? 0.0 : wall_clock_time / systemc_time);
         return EXIT_SUCCESS;
     } catch (sc_report& rep) {
         log_error("%s", rep.what());
