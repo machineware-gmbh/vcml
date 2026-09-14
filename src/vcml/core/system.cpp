e31363195718090b3c2801d157bfbd3518759c26
Niko Zurstraßen <niko@mwa.re>
2026-09-14 11:20:04 +0200

    fix

--- src/vcml/core/system.cpp
+++ src/vcml/core/system.cpp
@@ -136,7 +136,7 @@ int system::run() {
         log_info("elapsed SystemC time:      %.9fs", systemc_time);
         log_info("elapsed wall-clock time:   %.3fs", wall_clock_time);
         log_info("simulation realtime ratio: %.2fs / 1s",
-                 duration == 0.0 ? 0.0 : wall_clock_time / systemc_time);
+                 systemc_time == 0.0 ? 0.0 : wall_clock_time / systemc_time);
         return EXIT_SUCCESS;
     } catch (sc_report& rep) {
         log_error("%s", rep.what());
